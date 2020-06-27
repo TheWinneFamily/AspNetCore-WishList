cd78b91e60d936502c80dec4a3c144d05e897b54
Scott Winne <thewinnefamily@yahoo.com>
2020-06-27 09:35:45 -0400

    Next

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    class ApplicationDbContext : DbContext
+    {
+        public DBSet<Item> Item { get; set; }
+
+        public ApplicationDbContext( DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
