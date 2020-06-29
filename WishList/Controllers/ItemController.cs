0a1280587c04de75fe20c67c5b76cfbbd7ee76f8
Scott Winne <thewinnefamily@yahoo.com>
2020-06-28 20:57:40 -0400

    Update ItemController.cs

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -38,15 +38,10 @@ namespace WishList.Controllers
 
         public IActionResult Delete(int id)
         {
-            //public\s*IActionResult\s*Delete\s*?[(]\s*?int\s*id\s*?[)]\s*?{\s*?.*
             Item item = _context.Items.FirstOrDefault(i=> i.Id == id);
-            //_context.Items.FirstOrDefault[(].*[.]Id\s*?==\s*?id\s*?[)];\s*?
             _context.Items.Remove(item);
-            //_context([.]Items)?[.]Remove[(]\s*?.*\s*?[)];\s*?
             _context.SaveChanges();
-            //_context[.]SaveChanges[(]\s*?[)];\s*?
             return RedirectToAction("Index");
-            //return\s*RedirectToAction[(]""Index""(,\s*?""Item"")?[)];\s*?}";
         }
     }
 }
